155ff06c4ec7fa18b180a19d73bd20896a5b3c0d
Savitar97 <43468256+Savitar97@users.noreply.github.com>
2019-03-02 13:16:41 +0100

    Add files via upload

---
+++ szohossz/bitshift.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+unsigned int szam = 1;
+	int counter = 0;
+	while (szam <<=1){
+			cout<<szam<<'\n';
+			counter++;
+			
+	}
+
+	cout <<"Az egy "<< counter << " lepes utan lesz 0." << endl;
+
+return 0;
+}
