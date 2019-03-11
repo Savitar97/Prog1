63a7d55c089df49e4f2f0f24a75ed26d1badd43c
Savitar97 <43468256+Savitar97@users.noreply.github.com>
2019-03-12 00:05:02 +0100

    Add files via upload

---
+++ 3.6/signal.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void jelkezelo(int a)
+{
+	printf("AhAhAhAhAh!\n");
+}
+
+int main()
+{
+	for(;;)
+	{
+		if(signal(SIGINT, jelkezelo) == SIG_IGN)
+		{
+			signal(SIGINT,SIG_IGN);
+		}
+	}
+}
